13ee3d5064130405b0c0aca68fad48eedb38a007
taishikato <taishi.k0903@gmail.com>
2019-09-25 13:43:45 -0700

    :hammer: Make a post request

--- content.js
+++ content.js
@@ -1,11 +1,10 @@
-console.log('Open')
 var myInit = {
-  method: 'GET',
+  method: 'POST',
   mode: 'cors',
   cache: 'default'
 };
 const imageUrl = 'https://firebasestorage.googleapis.com/v0/b/the360gram.appspot.com/o/posts%2F3811c428c9354f01b1e56140468de806.png?alt=media&token=e22f7ade-178a-4de7-a964-3d49c19a7454'
-fetch('https://pinweb-enjoy.firebaseapp.com/s/takato2', myInit).then(res => {
+fetch('https://pinweb-enjoy.firebaseapp.com/getImageUrl', myInit).then(res => {
   console.log('success')
   console.log(res)
   return res.json()
